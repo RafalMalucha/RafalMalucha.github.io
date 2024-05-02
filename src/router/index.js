eadba1ca4c25fbf4438c2491960b9a1fe600ac72
Maksym Malichenko <s221680@student.uek.krakow.pl>
2024-05-03 01:43:18 +0200

    restriction for unauthorized users

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+/*
 import { createRouter, createWebHistory } from 'vue-router'
 import WelcomeView from '../views/WelcomeView.vue'
 import LoginView from '../views/LoginView.vue'
@@ -82,3 +83,83 @@ const router = createRouter({
 })
 
 export default router
+*/
+import { createRouter, createWebHistory } from 'vue-router';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import WelcomeView from '../views/WelcomeView.vue';
+import LoginView from '../views/LoginView.vue';
+import HomeView from '../views/HomeView.vue';
+import MenuView from '../views/MenuView.vue';
+import AccountView from '../views/AccountView.vue';
+import DeliveryView from '../views/DeliveryView.vue';
+import SummaryView from '../views/SummaryView.vue';
+import ThanksView from '../views/ThanksView.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Welcome',
+    component: WelcomeView
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: HomeView,
+    meta: { requiresAuth: true }  // Requires authentication
+  },
+  {
+    path: '/menu',
+    name: 'Menu',
+    component: MenuView,
+    meta: { requiresAuth: true }  // Requires authentication
+  },
+  {
+    path: '/delivery',
+    name: 'Delivery',
+    component: DeliveryView,
+    meta: { requiresAuth: true }  // Requires authentication
+  },
+  {
+    path: '/summary',
+    name: 'Summary',
+    component: SummaryView,
+    meta: { requiresAuth: true }  // Requires authentication
+  },
+  {
+    path: '/thanks',
+    name: 'Thanks',
+    component: ThanksView,
+    meta: { requiresAuth: true }  // Requires authentication
+  },
+  {
+    path: '/account',
+    name: 'Account',
+    component: AccountView,
+    meta: { requiresAuth: true }  // Requires authentication
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const auth = getAuth();
+  
+  onAuthStateChanged(auth, user => {
+    if (requiresAuth && !user) {
+      next({ name: 'Login' });  // Redirect to Login if not authenticated
+    } else {
+      next();  // Proceed to route
+    }
+  });
+});
+
+export default router;
