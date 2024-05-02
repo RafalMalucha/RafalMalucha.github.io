473a06856ec2a30f39d82dc72b36c292ee5c1e1c
m-warszawski <s223194@student.urk.krakow.pl>
2024-05-02 21:09:42 +0200

    Map files

--- src/components/MapArea.vue
+++ src/components/MapArea.vue
@@ -2,11 +2,63 @@
 </script>
 
 <template>
-    <div>
-        TUTAJ BĘDZIE MAPA
+    <div id="map-container">
+        <div ref="map" style="height: 400px;"></div>
     </div>
 </template>
 
+<script>
+// import { ref, onMounted } from 'vue';
+// import '../assets/leaflet.css';
+// import L from '../assets/leaflet.css';
+
+// export default {
+//     setup() {
+//         var map = ref(null);
+
+//         onMounted(() => {
+//             function success(pos) {
+//                 const crd = pos.coords;
+
+//                 console.log("Your current position is:");
+//                 console.log(`Latitude : ${crd.latitude}`);
+//                 console.log(`Longitude: ${crd.longitude}`);
+//                 console.log(`More or less ${crd.accuracy} meters.`);
+
+//                 map.value = L.map('map-container').setView([crd.latitude, crd.longitude], 13);
+
+//                 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//                     attribution: '© OpenStreetMap contributors'
+//                 }).addTo(map.value);
+
+//                 var ico = L.icon({
+//                     iconUrl: '../assets/icons/marker-icon-2x.png',
+//                 })
+
+//                 L.marker([crd.latitude, crd.longitude], { icon: ico }).addTo(map.value);
+//             }
+
+//             navigator.geolocation.getCurrentPosition(success,
+//                 position => {
+//                     console.log(position.coords.latitude);
+//                     // const lat = position.coords.latitude;
+//                     // const long = position.coords.longitude;
+//                 },
+//                 error => {
+//                     console.log(error.message);
+//                 },)
+//         });
+
+//         return { map };
+//     }
+// };
+</script>
+
+<style>
+#map-container {
+    height: 100%;
+}
+</style>
 <style scoped>
 div {
     height: 600px;
