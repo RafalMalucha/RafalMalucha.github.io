b7fb67109459171289fbc178c78fac3ac18f5926
Maksym Malichenko <s221680@student.uek.krakow.pl>
2024-05-03 12:27:05 +0200

    database change, routing for auth user

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,5 +1,23 @@
 <script setup>
-import AppLogo from '../components/images/AppLogo.vue'
+import AppLogo from '../components/images/AppLogo.vue';
+import { useRouter } from 'vue-router';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const router = useRouter();
+const auth = getAuth();
+
+const navigateUser = () => {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      // If user is logged in, redirect to Home
+      router.push({ name: 'Home' });
+    } else {
+      // If user is not logged in, redirect to Login
+      router.push({ name: 'Login' });
+    }
+  });
+};
+
 </script>
 
 <template>
@@ -8,9 +26,7 @@ import AppLogo from '../components/images/AppLogo.vue'
       <div class="row">
         <div class="col-md-8">
           <AppLogo />
-          <router-link :to="{ name: 'Login' }">
-            <button type="button" class="btn">Start</button>
-          </router-link>
+          <button type="button" class="btn" @click="navigateUser">Start</button>
         </div>
         <div class="col-md-4">
           <div id="authors">
@@ -38,7 +54,7 @@ main {
   padding-top: 150px;
 }
 
-.row>div:nth-of-type(1) {
+.row > div:nth-of-type(1) {
   text-align: center;
 }
 
@@ -47,11 +63,11 @@ main {
     padding-top: 50px;
   }
 
-  .row>div:nth-of-type(1) button {
+  .row > div:nth-of-type(1) button {
     margin-top: 100px;
   }
 
-  .row>div:nth-of-type(2) {
+  .row > div:nth-of-type(2) {
     text-align: center;
   }
 
